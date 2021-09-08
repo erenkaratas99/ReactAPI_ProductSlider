3016e8deef8d35efa03e7be89e13b58ec0334194
Eren Karataş <77201153+erenkaratas99@users.noreply.github.com>
2021-09-08 15:04:07 +0300

    Update box.js

--- src/Components/Box/box.js
+++ src/Components/Box/box.js
@@ -3,24 +3,34 @@ import React from "react";
 import './box.css';
 
 
-
-
 const Box = ({prod,cart,setCart}) => {
 
-
+    //global cart counter element that I used to count the array elements that have been added in to shopping cart by user
     let cart_cntr;
 
     const addCart = (val) => {
+        
+        //copying the real cart data (React state) and creating a backup (temp) data
         let backup = [...cart];
+        
+        //pushing the added product into backup data
         backup.push(val);
+        
+        //setting the updated (product-added) data into react state (cart)
         setCart(backup);
+        
+        //counting the number of elements in the cart
         cart_cntr = backup.length;
+        
+        //printing them into console log (the data can be used in variety of applications later)
         console.log(cart_cntr)
         console.log(backup)
 
     };
 
     return(
+        
+        //html part of the JSX
         <div className="ProdBox" >
 
 
